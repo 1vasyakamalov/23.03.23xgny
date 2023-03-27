1fdc8ca8e5bdc2fa17138278324555b7eceb7031
vasiliy Kamalov <vasya.kamalov.1993@gmail.com>
2023-03-27 13:16:58 +0700

    решили задачу task01

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите первое число!");
+int number1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число!");
+int number2 = Convert.ToInt32(Console.ReadLine());
+if (number1==number2*number2)
+{
+    {
+    Console.WriteLine($"Ура число {number1} являеться квадрантом {number2}");
+}
+
+}
+
+else
+{
+    Console.WriteLine($"число {number1} не являеться квадрантом {number2}");
+}
+    
+
