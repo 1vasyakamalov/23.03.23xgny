70f24fc95908fca8d55d84b60ac28933036a037d
vasiliy Kamalov <vasya.kamalov.1993@gmail.com>
2023-03-27 15:40:04 +0700

    решили задачу task03

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -4,11 +4,11 @@ int number1 = Convert.ToInt32(Console.ReadLine());
 
 Console.Write("Введите второе число!");
 int number2 = Convert.ToInt32(Console.ReadLine());
-if (number1==number2*number2)
+if (number1 == number2 * number2)
 {
     {
-    Console.WriteLine($"Ура число {number1} являеться квадрантом {number2}");
-}
+        Console.WriteLine($"Ура число {number1} являеться квадрантом {number2}");
+    }
 
 }
 
@@ -16,5 +16,5 @@ else
 {
     Console.WriteLine($"число {number1} не являеться квадрантом {number2}");
 }
-    
+
 
---
+++ Task03/Program.cs
@@ -0,0 +1,15 @@
+﻿/* Напишите программу,которая на вход принимет одно число (N), 
+а на выходе показывает все целые числа в промежутке от -N до N.
+4->"-4, -3, -2, -1, 0, 1, 2, 3, 4"
+2->"-2, -1, 0, 1, 2"*/
+
+Console.WriteLine("Введите положительное целое число!");
+int number = Convert.ToInt32("Console.WriteLine()");
+int count = -number;
+while (count <= number)
+{
+    Console.WriteLine("count");
+    
+    count++;
+}
+
